73ef4e7796a4200d4f1d7ef222096717d0bb0f31
Falah Institut <contact.falahinstitut@gmail.com>
2026-08-01 11:05:40 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({});
+export default defineConfig({
+  tanstackStart: {
+    server: {
+      preset: "static",
+      prerender: {
+        crawlLinks: true,
+        routes: ["/"]
+      }
+    }
+  }
+});
