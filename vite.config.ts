ab8431a2f0b5f4f8afff6d0f5a59ea05587e93d8
Falah Institut <contact.falahinstitut@gmail.com>
2026-08-01 10:59:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,3 @@
-// @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
-// or the app will break with duplicate plugins:
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  tanstackStart: {
-    // On désactive le SSR. 
-    // Cela force la création d'un fichier index.html standard (SPA) idéal pour GitHub Pages.
-    ssr: false, 
-  },
-});
+export default defineConfig({});
